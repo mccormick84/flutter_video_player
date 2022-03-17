fc40cf286f6de9528f04f1b9a6f0ed8324124924
Kenny <mccormick84@gmail.com>
2022-03-17 15:49:58 +0900

    플러터 비디오 플레이어 1

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  XFile? video;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: video == null ? renderEmpty() : renderVideo(),
+    );
+  }
+
+  Widget renderVideo() {
+    return Center(
+      child: Text('Video'),
+    );
+  }
+
+
+  Widget renderEmpty() {
+    return Container(
+      width: MediaQuery
+          .of(context)
+          .size
+          .width,
+      decoration: getBoxDecoration(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Logo(
+            onTap: onLogoTap,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          _AppName(),
+        ],
+      ),
+    );
+  }
+
+  BoxDecoration getBoxDecoration() {
+    return BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color(0xFF2A3A7C),
+          Color(0xFF000118),
+        ],
+      ),
+    );
+  }
+
+  void onLogoTap() async {
+    final ImagePicker _picker = ImagePicker();
+    final video = await _picker.pickVideo(
+        source: ImageSource.gallery,
+    );
+
+    if (video != null) {
+       setState(() {
+         this.video = video;
+       });
+    }
+  }
+}
+
+class _AppName extends StatelessWidget {
+  const _AppName({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 30,
+      fontWeight: FontWeight.w300,
+    );
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'VIDEO',
+          style: textStyle,
+        ),
+        Text(
+          'PLAYER',
+          style: textStyle.copyWith(
+            fontWeight: FontWeight.w700,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  final VoidCallback onTap;
+
+  const _Logo({required this.onTap, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Image.asset('asset/image/logo.png'),
+      onTap: onTap,
+    );
+  }
+}
